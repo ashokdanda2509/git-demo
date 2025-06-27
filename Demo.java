d50f8aca5273d81d49c213675463c0793ee442e1
Danda, Ashok - Contractor <adanda+EADev@contractor.ea.com>
2025-06-27 23:11:04 +0530

    message

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // This is a simple Java program that prints a message to the console.
+    }
+}
